23c6bbf17e269f54fa65e57b92ad5abfd823961d
ksiwon <pjo12346@kaist.ac.kr>
2025-01-22 04:55:15 +0900

    ready deploy

--- backend/index.js
+++ backend/index.js
@@ -311,9 +311,10 @@ app.delete("/api/notices/:LabName/:Index/comments/:CommentId", async (req, res) 
     }
 });
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
   
 app.listen(port, () => {
